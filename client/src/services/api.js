97947d7bba804e064e9493dd7e455959a0faf7f4
Unnais <ubsk2000@hw.ac.uk>
2026-08-15 21:33:48 +0530

    fix: use environment variable for API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import { store } from '../redux/store';
 import { logout } from '../redux/authSlice';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
